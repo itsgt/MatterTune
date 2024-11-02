b96d596501a7f603ce630868ae1c7ac2244bdda4
Nima Shoghi <nimashoghi@gmail.com>
2024-11-02 13:53:07 -0400

    Added back the deleted activation function implementation for JMP

--- src/mattertune/backbones/jmp.py
+++ src/mattertune/backbones/jmp.py
@@ -20,9 +20,41 @@ from ..finetune.base import (
     FinetuneModuleBaseConfig,
     ModelOutput,
 )
-from ..output_heads.layers.activation import get_activation_cls
 
 log = logging.getLogger(__name__)
+
+
+class ScaledSiLU(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.scale_factor = 1.0 / 0.6
+        self._activation = nn.SiLU()
+
+    @override
+    def forward(self, x):
+        return self._activation(x) * self.scale_factor
+
+
+def get_activation_cls(activation: str) -> type[nn.Module]:
+    """
+    Get the activation class from the activation name
+    """
+    activation = activation.lower()
+    if activation == "relu":
+        return nn.ReLU
+    elif activation == "silu" or activation == "swish":
+        return nn.SiLU
+    elif activation == "scaled_silu" or activation == "scaled_swish":
+        return ScaledSiLU
+    elif activation == "tanh":
+        return nn.Tanh
+    elif activation == "sigmoid":
+        return nn.Sigmoid
+    elif activation == "identity":
+        return nn.Identity
+    else:
+        raise ValueError(f"Activation {activation} is not supported")
 
 
 class JMPBackboneConfig(FinetuneModuleBaseConfig):
