3f92940da156fee10e3db4f2df4952cfb1bf3a8d
Nima Shoghi <nimashoghi@gmail.com>
2024-11-03 18:35:05 -0500

    Export registries from the init file

--- src/mattertune/__init__.py
+++ src/mattertune/__init__.py
@@ -2,3 +2,5 @@ from __future__ import annotations
 
 from .finetune.main import MatterTuner as MatterTuner
 from .finetune.main import MatterTunerConfig as MatterTunerConfig
+from .registry import backbone_registry as backbone_registry
+from .registry import data_registry as data_registry
