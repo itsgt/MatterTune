2a6fadb6d6a8740e3a5e3196137775e009e4851e
Lingyu-Kong <kongly677@gmail.com>
2024-11-07 14:33:19 -0500

    db, matbench, mp, mptraj, xyz passed testing

--- src/mattertune/configs/data/mp/__init__.py
+++ src/mattertune/configs/data/mp/__init__.py
@@ -5,3 +5,5 @@ __codegen__ = True
 from mattertune.data.mp import DatasetConfigBase as DatasetConfigBase
 from mattertune.data.mp import MPDatasetConfig as MPDatasetConfig
 from mattertune.data.mptraj import MPTrajDatasetConfig as MPTrajDatasetConfig
+
+from . import raj as raj
