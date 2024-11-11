294114b969fb99e552ebb1d56f70ad8c6c635318
Lingyu-Kong <kongly677@gmail.com>
2024-11-10 21:29:13 -0500

    add matbench examples

---
+++ examples/matbench/orb-finetune.py
@@ -0,0 +1,93 @@
+from __future__ import annotations
+
+import logging
+import os
+from pathlib import Path
+
+import nshutils as nu
+import pytorch_lightning as pl
+import rich
+from lightning.pytorch.callbacks import EarlyStopping, ModelCheckpoint
+from lightning.pytorch.loggers import WandbLogger
+
+import mattertune
+import mattertune.backbones
+import mattertune.configs as MC
+from mattertune import MatterTuner
+
+logging.basicConfig(level=logging.DEBUG)
+nu.pretty()
+
+
+def main(args_dict: dict):
+    def hparams():
+        hparams = MC.MatterTunerConfig.draft()
+
+        ## Model Hyperparameters
+        hparams.model = MC.ORBBackboneConfig.draft()
+        hparams.model.pretrained_model = args_dict["model_name"]
+        hparams.model.ignore_gpu_batch_transform_error = True
+        hparams.model.optimizer = MC.AdamWConfig(lr=args_dict["lr"])
+
+        hparams.model.properties = []
+        property = MC.GraphPropertyConfig(
+            loss=MC.MAELossConfig(),
+            loss_coefficient=1.0,
+            reduction="mean",
+            name=args_dict["property"],
+            dtype="float",
+        )
+        hparams.model.properties.append(property)
+
+        ## Data Hyperparameters
+        hparams.data = MC.AutoSplitDataModuleConfig.draft()
+        hparams.data.dataset = MC.XYZDatasetConfig.draft()
+        hparams.data.dataset.src = Path(args_dict["xyz_path"])
+        hparams.data.train_split = args_dict["train_split"]
+        hparams.data.batch_size = args_dict["batch_size"]
+        hparams.data.num_workers = 8
+
+        ## Trainer Hyperparameters
+        wandb_logger = WandbLogger(project="MatterTune-Examples", name="JMP-Water")
+        checkpoint_callback = ModelCheckpoint(
+            monitor="val/forces_mae",
+            dirpath="./checkpoints",
+            filename="jmp-best",
+            save_top_k=1,
+            mode="min",
+        )
+        early_stopping = EarlyStopping(
+            monitor="val/forces_mae", patience=200, mode="min"
+        )
+        hparams.lightning_trainer_kwargs = {
+            "max_epochs": args_dict["max_epochs"],
+            "accelerator": "gpu",
+            "devices": args_dict["devices"],
+            "strategy": "ddp",
+            "gradient_clip_algorithm": "value",
+            "gradient_clip_val": 1.0,
+            "precision": "bf16",
+            "inference_mode": False,
+            "logger": [wandb_logger],
+            "callbacks": [checkpoint_callback, early_stopping],
+        }
+
+        hparams = hparams.finalize(strict=False)
+        return hparams
+
+    mt_config = hparams()
+    model = MatterTuner(mt_config).tune()
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_name", type=str, default="orb-v2")
+    parser.add_argument("--batch_size", type=int, default=8)
+    parser.add_argument("--lr", type=float, default=8.0e-5)
+    parser.add_argument("--max_epochs", type=int, default=2000)
+    parser.add_argument("--devices", type=int, nargs="+", default=[0, 1, 2, 3])
+    args = parser.parse_args()
+    args_dict = vars(args)
+    main(args_dict)
