7ff86c487665cf135f0579fae4b28f4aa9c587e0
qzheng75 <zhengqianyu0227@gmail.com>
2024-11-25 06:37:59 -0800

    Remove type_dict python files to align with main branch's style

--- src/mattertune/configs/data/json/__init__.py
+++ src/mattertune/configs/data/json/__init__.py
@@ -4,16 +4,3 @@ __codegen__ = True
 
 from mattertune.data.json_data import DatasetConfigBase as DatasetConfigBase
 from mattertune.data.json_data import JSONDatasetConfig as JSONDatasetConfig
-
-from .DatasetConfigBase_typed_dict import (
-    CreateDatasetConfigBase as CreateDatasetConfigBase,
-)
-from .DatasetConfigBase_typed_dict import (
-    DatasetConfigBaseTypedDict as DatasetConfigBaseTypedDict,
-)
-from .JSONDatasetConfig_typed_dict import (
-    CreateJSONDatasetConfig as CreateJSONDatasetConfig,
-)
-from .JSONDatasetConfig_typed_dict import (
-    CreateJSONDatasetConfig as CreateJSONDatasetConfig,
-)
