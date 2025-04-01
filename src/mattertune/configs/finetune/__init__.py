47220ea0c71c3fc5ad1740e1a257efa00d0023e2
Ian Slagle <islagle3@gatech.edu>
2025-04-01 15:17:07 -0400

    Export GraphVectorPropertyConfig

--- src/mattertune/configs/finetune/__init__.py
+++ src/mattertune/configs/finetune/__init__.py
@@ -3,38 +3,44 @@ from __future__ import annotations
 __codegen__ = True
 
 from mattertune.finetune.base import (
-    FinetuneModuleBaseConfig as FinetuneModuleBaseConfig,
+    FinetuneModuleBaseConfig,
+    LRSchedulerConfig,
+    NormalizerConfig,
+    OptimizerConfig,
+    PropertyConfig,
 )
-from mattertune.finetune.base import LRSchedulerConfig as LRSchedulerConfig
-from mattertune.finetune.base import NormalizerConfig as NormalizerConfig
-from mattertune.finetune.base import OptimizerConfig as OptimizerConfig
-from mattertune.finetune.base import PropertyConfig as PropertyConfig
-from mattertune.finetune.loss import HuberLossConfig as HuberLossConfig
-from mattertune.finetune.loss import L2MAELossConfig as L2MAELossConfig
-from mattertune.finetune.loss import LossConfig as LossConfig
-from mattertune.finetune.loss import MAELossConfig as MAELossConfig
-from mattertune.finetune.loss import MSELossConfig as MSELossConfig
+
+from mattertune.finetune.loss import (
+    HuberLossConfig,
+    L2MAELossConfig,
+    LossConfig,
+    MAELossConfig,
+    MSELossConfig,
+)
+
 from mattertune.finetune.lr_scheduler import (
-    CosineAnnealingLRConfig as CosineAnnealingLRConfig,
+    CosineAnnealingLRConfig,
+    ExponentialConfig,
+    MultiStepLRConfig,
+    ReduceOnPlateauConfig,
+    StepLRConfig,
 )
-from mattertune.finetune.lr_scheduler import ExponentialConfig as ExponentialConfig
-from mattertune.finetune.lr_scheduler import MultiStepLRConfig as MultiStepLRConfig
-from mattertune.finetune.lr_scheduler import (
-    ReduceOnPlateauConfig as ReduceOnPlateauConfig,
+
+
+
+from mattertune.finetune.optimizer import (
+    AdamConfig,
+    AdamWConfig,
+    SGDConfig,
 )
-from mattertune.finetune.lr_scheduler import StepLRConfig as StepLRConfig
-from mattertune.finetune.optimizer import AdamConfig as AdamConfig
-from mattertune.finetune.optimizer import AdamWConfig as AdamWConfig
-from mattertune.finetune.optimizer import SGDConfig as SGDConfig
+
 from mattertune.finetune.properties import (
-    AtomInvariantVectorPropertyConfig as AtomInvariantVectorPropertyConfig,
-)
-from mattertune.finetune.properties import EnergyPropertyConfig as EnergyPropertyConfig
-from mattertune.finetune.properties import ForcesPropertyConfig as ForcesPropertyConfig
-from mattertune.finetune.properties import GraphPropertyConfig as GraphPropertyConfig
-from mattertune.finetune.properties import PropertyConfigBase as PropertyConfigBase
-from mattertune.finetune.properties import (
-    StressesPropertyConfig as StressesPropertyConfig,
+    AtomInvariantVectorPropertyConfig,
+    GraphVectorPropertyConfig,
+    GraphPropertyConfig,
+    EnergyPropertyConfig,
+    PropertyConfigBase,
+    StressesPropertyConfig,
 )
 
 from . import base as base
@@ -53,6 +59,7 @@ __all__ = [
     "FinetuneModuleBaseConfig",
     "ForcesPropertyConfig",
     "GraphPropertyConfig",
+    "GraphVectorPropertyConfig",
     "HuberLossConfig",
     "L2MAELossConfig",
     "LRSchedulerConfig",
