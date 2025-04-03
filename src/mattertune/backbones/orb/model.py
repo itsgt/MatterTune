7717e0e00a9be286669e82c3cfb0b5c09e355f97
Ian Slagle <islagle3@gatech.edu>
2025-04-03 11:43:50 -0400

    Add properties to orb

--- src/mattertune/backbones/orb/model.py
+++ src/mattertune/backbones/orb/model.py
@@ -175,6 +175,43 @@ class ORBBackboneModule(
                         ),
                         compute_stress=False,
                     )
+            case props.GraphVectorPropertyConfig():
+                with optional_import_error_message("orb-models"):
+                    from orb_models.forcefield.graph_regressor import GraphHead  # type: ignore[reportMissingImports] # noqa
+                    from orb_models.forcefield.property_definitions import (  # type: ignore[reportMissingImports] # noqa
+                        PropertyDefinition,
+                    )
+
+                return GraphHead(
+                    latent_dim=256,
+                    num_mlp_layers=1,
+                    mlp_hidden_dim=256,
+                    target=PropertyDefinition(
+                        name=prop.name,
+                        dim=prop.size,
+                        domain="real",
+                    ),
+                    compute_stress=False,
+                )
+            case props.AtomInvariantVectorPropertyConfig():
+                with optional_import_error_message("orb-models"):
+                    from orb_models.forcefield.graph_regressor import NodeHead  # type: ignore[reportMissingImports] # noqa
+                    from orb_models.forcefield.property_definitions import (  # type: ignore[reportMissingImports] # noqa
+                        PropertyDefinition,
+                    )
+
+                return NodeHead(
+                    latent_dim=256,
+                    num_mlp_layers=1,
+                    mlp_hidden_dim=256,
+                    target=PropertyDefinition(
+                        name=prop.name,
+                        dim=prop.size,
+                        domain="real",
+                    ),
+                    remove_mean=False,
+                    remove_torque_for_nonpbc_systems=False,
+                )
             case _:
                 raise ValueError(
                     f"Unsupported property config: {prop} for ORB"
