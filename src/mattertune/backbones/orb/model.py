9a3dbedaf78ae9f0e8bb11b697e44814cd00ca10
itsgt <islagle3@gatech.edu>
2025-05-09 20:45:38 -0400

    Remove new forward

--- src/mattertune/backbones/orb/model.py
+++ src/mattertune/backbones/orb/model.py
@@ -222,17 +222,17 @@ class ORBBackboneModule(
                     checkpoint=None,
                 )
 
-                def head_forward(
-                    self, batch
-                ) -> torch.Tensor:
-                    """Forward pass (without inverse transformation)."""
-                    input = segment_ops.aggregate_nodes(
-                        batch.node_features, batch.n_node, reduction=self.node_aggregation
-                    )
-                    pred = self.mlp(batch)
-                    return pred.squeeze(-1)
+                #def head_forward(
+                #    self, batch
+                #) -> torch.Tensor:
+                #    """Forward pass (without inverse transformation)."""
+                #    input = segment_ops.aggregate_nodes(
+                #        batch.node_features, batch.n_node, reduction=self.node_aggregation
+                #    )
+                #    pred = self.mlp(input)
+                #    return pred.squeeze(-1)
 
-                head.forward = head_forward
+                #head.forward = head_forward
 
                 return head
 
