9a3dd099e62da3ebd98d8ce94955317426f33534
Ian Slagle <islagle3@gatech.edu>
2026-05-29 15:36:38 -0400

    Remove other N_paths declaration

--- src/mattertune/backbones/orb/model.py
+++ src/mattertune/backbones/orb/model.py
@@ -626,7 +626,6 @@ class ORBBackboneModule(
 
             nk = len(paths_info[0][0]["k_feff"])
             N_abs = len(paths_info[struct_i])
-            atom_graphs.system_features["N_paths"] = torch.tensor([N_paths])
             atom_graphs.system_features["feff_k"] = paths_info[0][0]["k_feff"].to(device)
             energy_edges = torch.zeros((N_abs), device = device)
         
