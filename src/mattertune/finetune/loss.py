d92219b0cde7611aeec50e5c727400b6fc110c33
Ian Slagle <islagle3@gatech.edu>
2026-07-13 14:18:22 -0400

    Undo removal of second dimension

--- src/mattertune/finetune/loss.py
+++ src/mattertune/finetune/loss.py
@@ -276,7 +276,7 @@ def compute_loss_with_batch(
         case EXAFSLossConfig():
             edge_match = batch.system_features["edge_match"].int()
             if config.deltaR_only:
-                return F.mse_loss(config.offset_deltaR + config.scale_deltaR * prediction[edge_match], batch.system_features["deltar"], reduction=config.reduction)
+                return F.mse_loss(config.offset_deltaR + config.scale_deltaR * prediction[edge_match, 0], batch.system_features["deltar"], reduction=config.reduction)
             else:
                 return  ((1 / config.scale_deltaR ** 2) * F.mse_loss(config.offset_deltaR + config.scale_deltaR * prediction[edge_match, 0], batch.system_features["deltar"], reduction=config.reduction
                     ) + (1 / config.scale_sigma2 ** 2) * F.mse_loss(config.offset_sigma2 + config.scale_sigma2 * prediction[edge_match, 1], batch.system_features["sigma2"], reduction=config.reduction
