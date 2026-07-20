24cf376729bffe2684286f69b2c500ff7a9fb573
Ian Slagle <islagle3@gatech.edu>
2026-07-20 12:05:52 -0400

    Print some shapes to check

--- src/mattertune/finetune/loss.py
+++ src/mattertune/finetune/loss.py
@@ -285,7 +285,7 @@ def compute_loss_with_batch(
             edge_match = edge_match + edge_offsets[edge_match_id_mapped]
 
             assert torch.linalg.norm(batch.edge_features["vectors"][edge_match] - 
-                batch.system_features["edge_vec_check"], axis = 1).min() < 0.02
+                batch.system_features["edge_vec_check"], axis = 1).max() < 0.02, f'{N_edges.shape} {edge_offsets.shape} {edge_match.shape} {edge_match_id_mapped.shape}'
 
             if config.deltaR_only:
                 return F.mse_loss(config.offset_deltaR + config.scale_deltaR * prediction[edge_match, 0], batch.system_features["deltar"], reduction=config.reduction)
