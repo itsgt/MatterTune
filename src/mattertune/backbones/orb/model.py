4df1da63ee0d85917fc7cad3dd9f256a05937c97
Ian Slagle <islagle3@gatech.edu>
2026-07-22 15:23:54 -0400

    Keep dropout rate as int but make it percentage

--- src/mattertune/backbones/orb/model.py
+++ src/mattertune/backbones/orb/model.py
@@ -252,7 +252,7 @@ class ORBBackboneModule(
                         hidden_layer_sizes=[hidden_dim] * num_layers,
                         output_size=prop.size,
                         activation='silu',
-                        dropout=prop.additional_head_settings['dropout_rate'],
+                        dropout=prop.additional_head_settings['dropout_rate'] / 100.0,
                         checkpoint=None,
                     )
                 
