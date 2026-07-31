7bd4044cccc48a7a73d2b3afd8bea4240e0392f5
Ian Slagle <islagle3@gatech.edu>
2026-07-31 09:08:24 -0400

    Add Reffs, m_s, and m_a to the batch

--- src/mattertune/backbones/orb/model.py
+++ src/mattertune/backbones/orb/model.py
@@ -659,6 +659,9 @@ class ORBBackboneModule(
             sigma2 = torch.cat([(torch.tensor(paths_info[struct_i][j]["sigma2"], dtype=torch.float32)) for j in range(len(abs_inds[struct_i]))])
             third =  torch.cat([(torch.tensor(paths_info[struct_i][j]["third"],  dtype=torch.float32)) for j in range(len(abs_inds[struct_i]))])
             fourth = torch.cat([(torch.tensor(paths_info[struct_i][j]["fourth"], dtype=torch.float32)) for j in range(len(abs_inds[struct_i]))])
+            Reffs = torch.cat([(torch.tensor(paths_info[struct_i][j]["Reffs"], dtype=torch.float32)) for j in range(len(abs_inds[struct_i]))])
+            m_s = torch.cat([(torch.tensor(paths_info[struct_i][j]["m_s"], dtype=torch.float32)) for j in range(len(abs_inds[struct_i]))])
+            m_a = torch.cat([(torch.tensor(paths_info[struct_i][j]["m_a"], dtype=torch.float32)) for j in range(len(abs_inds[struct_i]))])
 
             assert len(edge_match[match_failed == 0]) / len(edge_match) > 0.95, f'Only kept {(100 * len(edge_match[match_failed == 0]) / len(edge_match)):.2f}% of paths for struct {struct_i}'
 
@@ -668,6 +671,9 @@ class ORBBackboneModule(
             atom_graphs.system_features["sigma2" ] = sigma2[match_failed == 0]
             atom_graphs.system_features["third" ] = third[match_failed == 0]
             atom_graphs.system_features["fourth"] = fourth[match_failed == 0]
+            atom_graphs.system_features["Reffs" ] = Reffs[match_failed == 0]
+            atom_graphs.system_features["m_s" ] = m_s[match_failed == 0]
+            atom_graphs.system_features["m_a"] = m_a[match_failed == 0]
             atom_graphs.system_features["struct_i"] = torch.tensor([struct_i], device = device)
             atom_graphs.system_features["N_edges"] = torch.tensor([N_edges], device = device)
             atom_graphs.system_features["edge_match_id"] = struct_i * torch.ones_like(edge_match[match_failed == 0])
